8302e62aaec80b4e68394b50e4c22417abb9daf5
Pratik Dhore <41249340+DrPratik@users.noreply.github.com>
2018-10-13 10:45:29 +0530

    Update 1studentdatabase.cpp

--- 1studentdatabase.cpp
+++ 1studentdatabase.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : ssd.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 #include <iostream>
 using namespace std;
 int m[31];
